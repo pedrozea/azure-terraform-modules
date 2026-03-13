eb79304d67b29e85e0f181967944487f8c14e9a5
Pedro Zea <pedrozea@live.com>
2026-03-13 18:02:09 +0100

    feat: Add subnet ID output to Linux VM module

--- modules/linux_vm/outputs.tf
+++ modules/linux_vm/outputs.tf
@@ -11,3 +11,8 @@ output "private_ip" {
   description = "Private IP address of the Linux VM"
   value       = azurerm_network_interface.nic.private_ip_address
 }
+
+output "subnet_id" {
+  description = "ID of the subnet where the VM is deployed"
+  value       = var.subnet_id
+}
