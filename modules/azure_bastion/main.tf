ab6e4097af867881a79a5432ec2973bcc07d8fa5
Pedro Zea <pedrozea@live.com>
2026-03-23 05:23:25 +0100

    feat: Add SKU and shareable link configuration to Azure Bastion

--- modules/azure_bastion/main.tf
+++ modules/azure_bastion/main.tf
@@ -20,14 +20,17 @@ resource "azurerm_public_ip" "bastion_pip" {
 # Azure Bastion
 # ------------------------------------------------------------------------------
 resource "azurerm_bastion_host" "bastion" {
-  name                = var.name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  tags                = var.tags
+  name                   = var.name
+  location               = var.location
+  resource_group_name    = var.resource_group_name
+  sku                    = var.sku
+  shareable_link_enabled = var.shareable_link_enabled
 
   ip_configuration {
     name                 = "configuration"
     subnet_id            = var.subnet_id
     public_ip_address_id = azurerm_public_ip.bastion_pip.id
   }
+
+  tags = var.tags
 }
--- modules/azure_bastion/outputs.tf
+++ modules/azure_bastion/outputs.tf
@@ -15,9 +15,15 @@ output "dns_name" {
   value       = azurerm_bastion_host.bastion.dns_name
 }
 
+output "bastion_sku" {
+  description = "The SKU of the Azure Bastion host."
+  value       = azurerm_bastion_host.bastion.sku
+}
+
 # ------------------------------------------------------------------------------
 # Public IP for Azure Bastion
 # ------------------------------------------------------------------------------
+
 output "public_ip_address" {
   description = "The public IP address of the Azure Bastion host."
   value       = azurerm_public_ip.bastion_pip.ip_address
